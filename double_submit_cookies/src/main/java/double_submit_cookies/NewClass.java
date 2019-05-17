7b2cd680b97ab5420961276089e559b722b3b5aa
dinuZgit <dinusdjay@gmail.com>
2019-05-18 00:02:39 +0530

    first commit

---
+++ double_submit_cookies/src/main/java/double_submit_cookies/NewClass.java
@@ -0,0 +1,14 @@
+package double_submit_cookies;
+
+public class NewClass {
+
+	public NewClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
